e58999fbc7c17f7529bfd8eb9dfb94165004cc88
Zhu Zhanyan <program.nom@gmail.com>
2020-09-26 13:16:23 +0800

    Configure terraform to use remote state.

--- tf/main.tf
+++ tf/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-
+## Remote Terraform State
 # S3 Bucket for storing terraform remote state
 resource "aws_s3_bucket" "tf_state_bucket" {
   bucket = "cloud-ws-tf-remote-state"
@@ -22,3 +22,16 @@ resource "aws_s3_bucket" "tf_state_bucket" {
     Project = "cloud-ws"
   }
 }
+
+# Configure terraform to use remote state
+terraform {
+  backend "s3" {
+    # point to bucket
+    bucket         = "cloud-ws-tf-remote-state"
+    key            = "tfstate/tf.tfstate"
+    region         = "ap-southeast-1"
+
+    # optional: dynamodb_table for locking ...
+  }
+}
+
